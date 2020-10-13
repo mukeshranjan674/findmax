03675ff16d6429d5564e5c973423553e8eee308f
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-13 16:21:11 +0530

    [Mukesh] add : Find maximum class written

---
+++ src/main/java/generics/findmax/FindMaximum.java
@@ -0,0 +1,13 @@
+package generics.findmax;
+
+/**
+ * Hello world!
+ *
+ */
+public class FindMaximum 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/generics/findmax/FindMaximumTest.java
@@ -0,0 +1,16 @@
+package generics.findmax;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class FindMaximumTest 
+{
+  
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
